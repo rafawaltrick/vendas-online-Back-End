6e307947c00442777961789461621c5e434cf264
rafawaltrick <rafael.waltrick@fatec.sp.gov.br>
2023-07-06 10:24:29 -0300

    crido o metodo post

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
-    @Get()
-    async getAllUser() {
-        return JSON.stringify({test: 'ABC'})
+    @Post()
+    async createUser(
+        @Body() createUser: CreateUserDto
+    ) {
+        return {
+            ...createUser,
+            password: undefined,
+        }
     }        
 }
