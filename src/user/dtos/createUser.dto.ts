1f4b753e22dde943688e091c811cba3d61a78603
rafawaltrick <rafael.waltrick@fatec.sp.gov.br>
2023-07-31 15:38:07 -0300

    Validação de requisição Post

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -1,7 +1,14 @@
-export interface CreateUserDto {
-    name: string;
-    email: string;
-    phone: string;
-    cpf: string;
-    password: string;
+import { IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+  @IsString()
+  email: string;
+  @IsString()
+  phone: string;
+  @IsString()
+  cpf: string;
+  @IsString()
+  password: string;
 }
